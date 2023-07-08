7bf233fe37fefec059a7b3a5b748404b6fa71967
Matheus Melo <mattmelo99@gmail.com>
2023-07-08 01:22:03 -0300

    Adiciona liquibase migrations para cada modulo

--- tutor/src/main/java/br/com/heypet/tutor/config/TutorPropsConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.heypet.tutor.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:tutor.properties")
-public class TutorPropsConfig {
-}
--- vet/src/main/java/br/com/heypet/vet/config/VetPropsConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.heypet.vet.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:vet.properties")
-public class VetPropsConfig {
-}
