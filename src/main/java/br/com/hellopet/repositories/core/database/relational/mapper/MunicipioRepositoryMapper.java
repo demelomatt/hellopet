1ba5cf6950e9822a0bc6e1bbd0439dd4e84b8dac
Matheus Melo <mattmelo99@gmail.com>
2023-08-06 16:26:48 -0300

    fix(crud-tutor) fix ajustes apontados pelo sonar

--- src/main/java/br/com/hellopet/repositories/core/database/relational/mapper/MunicipioRepositoryMapper.java
+++ src/main/java/br/com/hellopet/repositories/core/database/relational/mapper/MunicipioRepositoryMapper.java
@@ -1,11 +1,9 @@
 package br.com.hellopet.repositories.core.database.relational.mapper;
 
-import br.com.hellopet.repositories.core.database.relational.model.MunicipioModel;
 import br.com.hellopet.domain.core.entity.Municipio;
+import br.com.hellopet.repositories.core.database.relational.model.MunicipioModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-
-import java.util.List;
 
 @Mapper
 public interface MunicipioRepositoryMapper {
